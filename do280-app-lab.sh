357a70ac333212c187e2ea1d15a942a6b763c461
D Ther <dtherhtun.cw@gmail.com>
2020-12-21 20:06:00 +0630

    modify app

--- do280-app-lab.sh
+++ do280-app-lab.sh
@@ -13,7 +13,8 @@ case "$1" in
 	start)
 		echo -e "${YE}[*]${NC} Starting Lab......"
 		sleep 2
-        for pj in dev qa stag prod; do oc create project $pj; done
+        for pj in dev qa stag prod; do oc new-project $pj 1>/dev/null; done
+        echo -e "${YE}[*]${NC} Objective \n- Create an application name ${CY}cake${NC} in ${OR}dev${NC} using this https://github.com/sclorg/cakephp-ex sources and expose application to public.\n- Create an application name ${CY}cheese-cake${NC} in ${OR}qa${NC} project using same sources and expose application to public with service name ${BL}cake-qa${NC}.\n- Create an application name ${CY}choco-cake${NC} in ${OR}stag${NC} project using same sources and expose application to public with ${BL}choco-cake${NC} subdomain with ${CY}custom${NC} tls.\n- Create an application name ${CY}milk-cake${NC} in ${OR}prod${NC} project using same sources and create service account called ${CY}cake${NC} with edit role."
         ;;
     grade)
         echo hello
